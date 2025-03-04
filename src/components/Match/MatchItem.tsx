9af9e15d8ef58060d415eefd0a36f4630a132783
Aleksey <tangychannel@gmail.com>
2025-03-04 16:07:37 +0400

    feat: open card with animation

--- src/components/Match/MatchItem.tsx
+++ src/components/Match/MatchItem.tsx
@@ -4,6 +4,7 @@ import TeamStats from "@/components/Match/TeamStats";
 import { Icon, MatchBadge } from "../ui";
 
 import { Match } from "@/types/match";
+import { useState } from "react";
 
 interface Props {
   match: Match;
@@ -12,8 +13,15 @@ interface Props {
 const MatchItem = ({ match }: Props) => {
   const { homeTeam, awayTeam } = match;
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => setIsOpen((prev) => !prev);
+
   return (
-    <div className="bg-card rounded-sm px-10 py-4">
+    <div
+      className="bg-card rounded-sm px-10 py-4 cursor-pointer relative"
+      onClick={toggleOpen}
+    >
       <div className="flex justify-between items-center">
         <div className="flex gap-3.5 items-center">
           <Icon name="crown" />
@@ -31,13 +39,26 @@ const MatchItem = ({ match }: Props) => {
         </div>
       </div>
 
-      <div className="mt-11">
-        <TeamPlayers homeTeam={homeTeam} awayTeam={awayTeam} />
-        <div className="flex justify-between gap-8 mt-2">
-          <TeamStats team={homeTeam} />
-          <TeamStats team={awayTeam} />
+      <div
+        className={`transition-all duration-500 ease-linear ${
+          isOpen ? "max-h-screen" : "max-h-0 overflow-hidden"
+        }`}
+      >
+        <div className="mt-11">
+          <TeamPlayers homeTeam={homeTeam} awayTeam={awayTeam} />
+          <div className="flex justify-between gap-8 mt-2">
+            <TeamStats team={homeTeam} />
+            <TeamStats team={awayTeam} />
+          </div>
         </div>
       </div>
+
+      <Icon
+        name="chevron"
+        className={`absolute top-9 right-2 transition-transform ${
+          isOpen ? "rotate-180" : ""
+        }`}
+      />
     </div>
   );
 };
