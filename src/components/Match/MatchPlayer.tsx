a591f000b94bf150df707273754265eab929bfa5
Aleksey <tangychannel@gmail.com>
2025-03-04 16:22:07 +0400

    feat: add user avatar

--- src/components/Match/MatchPlayer.tsx
+++ src/components/Match/MatchPlayer.tsx
@@ -1,13 +1,18 @@
 import { Player } from "@/types/match";
 
+import PlayerAvatar from "@/assets/images/avatar.png";
+
 type Props = {
   player: Player;
 };
 
 const MatchPlayer = ({ player }: Props) => {
   return (
-    <div className="bg-muted flex justify-between px-6 py-4 max-w-72 w-full rounded-sm">
-      <p className="text-base font-semibold">{player.username}</p>
+    <div className="bg-muted flex justify-between px-3 py-4 w-full rounded-sm">
+      <div className="flex items-center gap-2">
+        <img src={PlayerAvatar} alt="Avatar" />
+        <p className="text-base font-semibold">{player.username}</p>
+      </div>
       <p className="text-[#FAFAFA66] text-sm flex justify-center items-center gap-1">
         Убийств:{" "}
         <span className="text-white text-base font-semibold">
